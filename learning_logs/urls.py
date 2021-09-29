f04fbf3bd30b832b37f25e72f2aed4df1c38f9f4
bruiseralmighty <56087757+bruiseralmighty@users.noreply.github.com>
2021-09-29 00:27:41 -0700

    finished chapter

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,4 +6,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page.
     path('', views.index, name='index'),
+    # Topics page to show all topics.
+    path('topics/', views.topics, name='topics'),
+    # View and individual topic.
+    path('topics/<int:topic_id>/', views.topic, name='topics')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(requests):
     """The home page for Learning Logs."""
     return render(requests, 'learning_logs/index.html')
+
+def topics(requests):
+    """Show all Topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(requests, 'learning_logs/topics.html', context)
+
+def topic(requests, topic_id):
+    """Show a single topic"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(requests, 'learning_logs/topic.html', context)
