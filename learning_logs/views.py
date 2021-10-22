cde1f9c1b083ef53b5233eeaf177f6bb95bbe134
edmoren <ejmoreno270@gmail.com>
2021-10-21 23:08:42 -0600

    using get_object_or_404 for topic grab

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
@@ -25,7 +25,7 @@ def topics(requests):
 @login_required
 def topic(requests, topic_id):
     """Show a single topic"""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     check_topic_owner(topic, requests.user)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
